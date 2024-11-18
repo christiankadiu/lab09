646efa7f7cf6ce412e9836563ce74727d5c4c717
Christian Kadiu - christian.kadiu@studio.unibo.it <christian.kadiu@studio.unibo.it>
2024-11-18 12:39:39 +0100

    implemented Controller.java and SimpleGUI.java

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,39 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    private static final String PATH = System.getProperty("user.home")
+    + File.separator
+    + Controller.class.getSimpleName() + ".txt";
+
+    File currentFile;
+
+    public Controller(){
+        currentFile = new File(PATH);
+    }
+
+    public void setCurrentFile(final File fileName){
+        currentFile = fileName;
+    }
+
+    public String getPath(){
+        return currentFile.getAbsolutePath();
+    }
+
+    public void writeFile(final String string){
+        try (PrintStream ps = new PrintStream(PATH, StandardCharsets.UTF_8)) {
+            ps.print(string);
+        } catch (IOException e1) {
+            e1.printStackTrace(); // NOPMD: allowed as this is just an exercise
+        }
+    }
 
 }
--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,18 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * A very simple program using a graphical interface.
@@ -9,5 +21,53 @@ import javax.swing.JFrame;
 public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
+    private static final int PROPORTION = 5;
+
+    SimpleGUI() {
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+        final JTextArea textArea = new JTextArea();
+        canvas.add(textArea, BorderLayout.CENTER);
+        final JButton button = new JButton("save");
+        canvas.add(button, BorderLayout.SOUTH);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                
+            }
+        });
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void display() {
+        /*
+         * Make the frame one fifth the resolution of the screen. This very method is
+         * enough for a single screen setup. In case of multiple monitors, the
+         * primary is selected. In order to deal coherently with multimonitor
+         * setups, other facilities exist (see the Java documentation about this
+         * issue). It is MUCH better than manually specify the size of a window
+         * in pixel: it takes into account the current resolution.
+         */
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        frame.pack();
+        /*
+         * Instead of appearing at (0,0), upper left corner of the screen, this
+         * flag makes the OS window manager take care of the default positioning
+         * on screen. Results may vary, but it is generally the best choice.
+         */
+        frame.setLocationByPlatform(true);
+        /*
+         * OK, ready to push the frame onscreen
+         */
+        frame.setVisible(true);
+    }
+
+    public static void main(final String... args) {
+        new SimpleGUI().display();
+     }
 
 }
