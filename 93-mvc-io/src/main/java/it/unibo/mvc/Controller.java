fb363bce723ded63becf178d88a5f775bbb26254
christian <kadiuchristian8@gmail.com>
2024-11-18 20:56:15 -0800

    made some changes to exercise 92 and partially solved exercise 93

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,43 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
+
+    /**
+     * sets the next string.
+     * 
+     * @param s the value of the string to set
+     * @throws nullpointerexeption if the string is null
+     */
+
+    public void setNextString(String s);
+
+    /**
+     * returns the next string to print.
+     * 
+     * @return String
+     */
+
+    public String getNexString();
+
+    /**
+     * gets the history of strings printed.
+     * 
+     * @return List
+     */
+
+    public List<String> getHistory();
+
+    /**
+     * prints the current string.
+     * 
+     * @throws IllegalStateException if the string is unset
+     */
+
+    public void printCurrentString();
 
 }
--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,47 @@
 package it.unibo.mvc;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+
+    private List<String> lista;
+    private String currentString;
+    private String next;
+
+    SimpleController() {
+        lista = new LinkedList<String>();
+    }
+
+    @Override
+    public void setNextString(String s) {
+        Objects.requireNonNull(s);
+        next = s;
+        lista.add(s);
+    }
+
+    @Override
+    public String getNexString() {
+        return next;
+    }
+
+    @Override
+    public List<String> getHistory() {
+        return lista;
+    }
+
+    @Override
+    public void printCurrentString() {
+        try {
+            System.out.println(currentString);
+        } catch (IllegalStateException e) {
+            e.getMessage().isBlank();
+        }
+    }
 
 }
